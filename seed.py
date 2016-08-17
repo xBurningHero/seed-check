5fce6877620fa91876e11b76aa5008c3b181022a
Chuck Mullins <chuck@resolvity.com>
2016-08-17 16:19:31 -0500

    Initial commit

--- seed.py
+++ seed.py
@@ -7,7 +7,7 @@ def testSeed(source):
   val = random.randint(0,6)
   print source,' has seed value of: ',val
 
-hosts = ['rt1.dal.resolvity.com','rt2.dal.resolvity.com','rt1.las.resolvity.com','rt2.las.resolvity.com']
+host['list','of','hostnames']
 
 for host in hosts:
   testSeed(host)
